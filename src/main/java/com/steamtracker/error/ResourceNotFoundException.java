7c6880bf67c67f25b31047d5673c619086f8400f
Baptistebsnt <112467099+Baptistebsnt@users.noreply.github.com>
2026-06-26 11:50:47 +0200

    feat: Implement global exception handling and custom error responses (#11)

---
+++ src/main/java/com/steamtracker/error/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.steamtracker.error;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
